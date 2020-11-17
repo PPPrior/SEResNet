8f54c6555c501e96549a820d643d6923fbedb69d
PPPrior <1256961901@qq.com>
2020-11-17 16:43:43 +0800

    Initial commit

---
+++ model/se.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+
+
+class SE(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SE, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channels // reduction, channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        out = self.avg_pool(x).view(b, c)
+        out = self.fc(out).view(b, c, 1, 1)
+        return x * out.expand_as(x)
